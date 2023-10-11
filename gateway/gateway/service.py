ca6d4c4005470848b940500b2a9da5c1a6f9e84d
alissao <alissonhbv@gmail.com>
2023-10-11 02:58:11 -0300

    Feat: Enhancing order service by not loading products unnecessarily.

--- gateway/gateway/service.py
+++ gateway/gateway/service.py
@@ -104,19 +104,16 @@ class GatewayService(object):
         # raise``OrderNotFound``
         order = self.orders_rpc.get_order(order_id)
 
-        # Retrieve all products from the products service
-        product_map = {prod['id']: prod for prod in self.products_rpc.list()}
-
         # get the configured image root
         image_root = config['PRODUCT_IMAGE_ROOT']
 
         # Enhance order details with product and image details.
         for item in order['order_details']:
-            product_id = item['product_id']
+            product = self.products_rpc.get(item['product_id'])
 
-            item['product'] = product_map[product_id]
+            item['product'] = product
             # Construct an image url.
-            item['image'] = '{}/{}.jpg'.format(image_root, product_id)
+            item['image'] = '{}/{}.jpg'.format(image_root, product['id'])
 
         return order
 
@@ -167,10 +164,8 @@ class GatewayService(object):
         return Response(json.dumps({'id': id_}), mimetype='application/json')
 
     def _create_order(self, order_data):
-        # check order product ids are valid
-        valid_product_ids = {prod['id'] for prod in self.products_rpc.list()}
         for item in order_data['order_details']:
-            if item['product_id'] not in valid_product_ids:
+            if self.products_rpc.get(item['product_id']) == None:
                 raise ProductNotFound(
                     "Product Id {}".format(item['product_id'])
                 )
