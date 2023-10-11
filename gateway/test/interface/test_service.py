f84e1bc45595e8cd01733e9a90a453eb2751c207
alissao <alissonhbv@gmail.com>
2023-10-11 18:56:52 -0300

    Fixture: Adding unit test for Deleting product, and fixing test for listing orders.

--- gateway/test/interface/test_service.py
+++ gateway/test/interface/test_service.py
@@ -89,12 +89,6 @@ class TestGetOrder(object):
             'id': 1,
             'order_details': [
                 {
-                    'id': 1,
-                    'quantity': 2,
-                    'product_id': 'the_odyssey',
-                    'price': '200.00'
-                },
-                {
                     'id': 2,
                     'quantity': 1,
                     'product_id': 'the_enigma',
@@ -104,22 +98,13 @@ class TestGetOrder(object):
         }
 
         # setup mock products-service response:
-        gateway_service.products_rpc.list.return_value = [
-            {
-                'id': 'the_odyssey',
-                'title': 'The Odyssey',
-                'maximum_speed': 3,
-                'in_stock': 899,
-                'passenger_capacity': 100
-            },
-            {
-                'id': 'the_enigma',
-                'title': 'The Enigma',
-                'maximum_speed': 200,
-                'in_stock': 1,
-                'passenger_capacity': 4
-            },
-        ]
+        gateway_service.products_rpc.get.return_value = {
+            'id': 'the_enigma',
+            'title': 'The Enigma',
+            'maximum_speed': 200,
+            'in_stock': 1,
+            'passenger_capacity': 4
+        }
 
         # call the gateway service to get order #1
         response = web_session.get('/orders/1')
@@ -128,21 +113,6 @@ class TestGetOrder(object):
         expected_response = {
             'id': 1,
             'order_details': [
-                {
-                    'id': 1,
-                    'quantity': 2,
-                    'product_id': 'the_odyssey',
-                    'image':
-                        'http://example.com/airship/images/the_odyssey.jpg',
-                    'product': {
-                        'id': 'the_odyssey',
-                        'title': 'The Odyssey',
-                        'maximum_speed': 3,
-                        'in_stock': 899,
-                        'passenger_capacity': 100
-                    },
-                    'price': '200.00'
-                },
                 {
                     'id': 2,
                     'quantity': 1,
@@ -164,7 +134,7 @@ class TestGetOrder(object):
 
         # check dependencies called as expected
         assert [call(1)] == gateway_service.orders_rpc.get_order.call_args_list
-        assert [call()] == gateway_service.products_rpc.list.call_args_list
+        assert [call('the_enigma')] == gateway_service.products_rpc.get.call_args_list
 
     def test_order_not_found(self, gateway_service, web_session):
         gateway_service.orders_rpc.get_order.side_effect = (
@@ -292,3 +262,20 @@ class TestCreateOrder(object):
         assert response.status_code == 404
         assert response.json()['error'] == 'PRODUCT_NOT_FOUND'
         assert response.json()['message'] == 'Product Id unknown'
+
+
+class TestDeleteProduct(object):
+    def test_can_delete_product(self, gateway_service, web_session):
+        response = web_session.delete('/products/the_odyssey')
+        assert response.status_code == 200
+
+    def test_delete_product_not_found(self, gateway_service, web_session):
+        gateway_service.products_rpc.delete.side_effect = (
+            ProductNotFound('missing'))
+
+        # call the gateway service to get order #1
+        response = web_session.delete('/products/foo')
+        assert response.status_code == 404
+        payload = response.json()
+        assert payload['error'] == 'PRODUCT_NOT_FOUND'
+        assert payload['message'] == 'missing'
